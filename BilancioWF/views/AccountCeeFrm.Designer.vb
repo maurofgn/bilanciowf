9d95a73e18d2cea861ed2d3dc2596cba9888491c
Mauro Fugante <mauro.fugante@gmail.com>
2015-11-20 17:34:39 +0100

    i

---
+++ BilancioWF/views/AccountCeeFrm.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AccountCeeFrm
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxCode = New System.Windows.Forms.TextBox()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxSeqNo = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.CheckBoxActive = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxParent = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxTotal = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxDebit = New System.Windows.Forms.CheckBox()
+        Me.ComboBoxParent = New System.Windows.Forms.ComboBox()
+        Me.ButtonNew = New System.Windows.Forms.Button()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.ButtonDel = New System.Windows.Forms.Button()
+        Me.ButtonSearch = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(3, 3)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(445, 444)
+        Me.TreeView1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(456, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Codice"
+        '
+        'TextBoxCode
+        '
+        Me.TextBoxCode.Location = New System.Drawing.Point(508, 12)
+        Me.TextBoxCode.Name = "TextBoxCode"
+        Me.TextBoxCode.Size = New System.Drawing.Size(134, 20)
+        Me.TextBoxCode.TabIndex = 1
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(507, 41)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(252, 20)
+        Me.TextBoxName.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(456, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Nome"
+        '
+        'TextBoxSeqNo
+        '
+        Me.TextBoxSeqNo.Location = New System.Drawing.Point(508, 67)
+        Me.TextBoxSeqNo.Name = "TextBoxSeqNo"
+        Me.TextBoxSeqNo.Size = New System.Drawing.Size(134, 20)
+        Me.TextBoxSeqNo.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(456, 70)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Sequenz."
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(456, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Padre"
+        '
+        'CheckBoxActive
+        '
+        Me.CheckBoxActive.AutoSize = True
+        Me.CheckBoxActive.Checked = True
+        Me.CheckBoxActive.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBoxActive.Location = New System.Drawing.Point(631, 148)
+        Me.CheckBoxActive.Name = "CheckBoxActive"
+        Me.CheckBoxActive.Size = New System.Drawing.Size(53, 17)
+        Me.CheckBoxActive.TabIndex = 8
+        Me.CheckBoxActive.Text = "Attivo"
+        Me.CheckBoxActive.UseVisualStyleBackColor = True
+        '
+        'CheckBoxParent
+        '
+        Me.CheckBoxParent.AutoSize = True
+        Me.CheckBoxParent.Enabled = False
+        Me.CheckBoxParent.Location = New System.Drawing.Point(454, 148)
+        Me.CheckBoxParent.Name = "CheckBoxParent"
+        Me.CheckBoxParent.Size = New System.Drawing.Size(54, 17)
+        Me.CheckBoxParent.TabIndex = 5
+        Me.CheckBoxParent.Text = "Padre"
+        Me.CheckBoxParent.UseVisualStyleBackColor = True
+        '
+        'CheckBoxTotal
+        '
+        Me.CheckBoxTotal.AutoSize = True
+        Me.CheckBoxTotal.Enabled = False
+        Me.CheckBoxTotal.Location = New System.Drawing.Point(514, 148)
+        Me.CheckBoxTotal.Name = "CheckBoxTotal"
+        Me.CheckBoxTotal.Size = New System.Drawing.Size(56, 17)
+        Me.CheckBoxTotal.TabIndex = 6
+        Me.CheckBoxTotal.Text = "Totale"
+        Me.CheckBoxTotal.UseVisualStyleBackColor = True
+        '
+        'CheckBoxDebit
+        '
+        Me.CheckBoxDebit.AutoSize = True
+        Me.CheckBoxDebit.Enabled = False
+        Me.CheckBoxDebit.Location = New System.Drawing.Point(576, 148)
+        Me.CheckBoxDebit.Name = "CheckBoxDebit"
+        Me.CheckBoxDebit.Size = New System.Drawing.Size(49, 17)
+        Me.CheckBoxDebit.TabIndex = 7
+        Me.CheckBoxDebit.Text = "Dare"
+        Me.CheckBoxDebit.UseVisualStyleBackColor = True
+        '
+        'ComboBoxParent
+        '
+        Me.ComboBoxParent.FormattingEnabled = True
+        Me.ComboBoxParent.Location = New System.Drawing.Point(508, 98)
+        Me.ComboBoxParent.Name = "ComboBoxParent"
+        Me.ComboBoxParent.Size = New System.Drawing.Size(251, 21)
+        Me.ComboBoxParent.TabIndex = 4
+        '
+        'ButtonNew
+        '
+        Me.ButtonNew.Enabled = False
+        Me.ButtonNew.Location = New System.Drawing.Point(459, 400)
+        Me.ButtonNew.Name = "ButtonNew"
+        Me.ButtonNew.Size = New System.Drawing.Size(63, 24)
+        Me.ButtonNew.TabIndex = 9
+        Me.ButtonNew.Text = "Nuovo"
+        Me.ButtonNew.UseVisualStyleBackColor = True
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Enabled = False
+        Me.ButtonSave.Location = New System.Drawing.Point(528, 400)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(62, 24)
+        Me.ButtonSave.TabIndex = 10
+        Me.ButtonSave.Text = "Salva"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'ButtonDel
+        '
+        Me.ButtonDel.Enabled = False
+        Me.ButtonDel.Location = New System.Drawing.Point(596, 400)
+        Me.ButtonDel.Name = "ButtonDel"
+        Me.ButtonDel.Size = New System.Drawing.Size(62, 24)
+        Me.ButtonDel.TabIndex = 11
+        Me.ButtonDel.Text = "Elimina"
+        Me.ButtonDel.UseVisualStyleBackColor = True
+        '
+        'ButtonSearch
+        '
+        Me.ButtonSearch.Location = New System.Drawing.Point(664, 400)
+        Me.ButtonSearch.Name = "ButtonSearch"
+        Me.ButtonSearch.Size = New System.Drawing.Size(62, 24)
+        Me.ButtonSearch.TabIndex = 12
+        Me.ButtonSearch.Text = "Cerca"
+        Me.ButtonSearch.UseVisualStyleBackColor = True
+        '
+        'AccountCeeFrm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(780, 453)
+        Me.Controls.Add(Me.ButtonSearch)
+        Me.Controls.Add(Me.ButtonDel)
+        Me.Controls.Add(Me.ButtonSave)
+        Me.Controls.Add(Me.ButtonNew)
+        Me.Controls.Add(Me.ComboBoxParent)
+        Me.Controls.Add(Me.CheckBoxDebit)
+        Me.Controls.Add(Me.CheckBoxTotal)
+        Me.Controls.Add(Me.CheckBoxParent)
+        Me.Controls.Add(Me.CheckBoxActive)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBoxSeqNo)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxCode)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Name = "AccountCeeFrm"
+        Me.Text = "Conti cee"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxCode As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxSeqNo As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents CheckBoxActive As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxParent As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxTotal As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxDebit As System.Windows.Forms.CheckBox
+    Friend WithEvents ComboBoxParent As System.Windows.Forms.ComboBox
+    Friend WithEvents ButtonNew As System.Windows.Forms.Button
+    Friend WithEvents ButtonSave As System.Windows.Forms.Button
+    Friend WithEvents ButtonDel As System.Windows.Forms.Button
+    Friend WithEvents ButtonSearch As System.Windows.Forms.Button
+End Class
---
+++ BilancioWF/views/AccountCeeFrm.vb
@@ -0,0 +1,336 @@
+﻿Imports System.Data.OleDb
+Imports System.Text
+Imports BilancioWF.Models
+Imports System.Text.RegularExpressions
+
+Public Class AccountCeeFrm
+    Implements Traverse
+
+    Dim allNodes As Dictionary(Of Integer, AccountCee)
+    Private Property currentNode As AccountCee
+
+    Private controller As AccountCeeController = New AccountCeeController()
+
+    Private Sub ContoCee_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
+
+        controller.dispose()
+
+    End Sub
+
+    Private Sub ContoCee_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        allNodes = controller.loadAall(False)
+        populateTree()
+        PopulateCombo(ComboBoxParent, controller.getSummary(allNodes.Values), "id", "codeName")
+
+        TreeView1.ExpandAll()
+
+    End Sub
+
+    Private Sub populateTree()
+
+        allNodes.Values.ToList.ForEach(Sub(oneRow) AddNode(oneRow))
+
+    End Sub
+
+    Private Sub AddNode(oneRow As AccountCee)
+
+        Dim nodes As New List(Of TreeNode)
+
+        nodes.AddRange(TreeView1.Nodes.Find(oneRow.ParentID.ToString, True))
+
+        Dim n As TreeNode = New TreeNode()
+        n.Text = oneRow.CodeName
+        n.Name = oneRow.ID.ToString
+        n.Tag = oneRow
+
+        If Not nodes.Any Then
+            TreeView1.Nodes.Add(n)
+        Else
+            nodes(0).Nodes.Add(n)
+        End If
+    End Sub
+
+    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
+
+        Try
+            Dim key As Integer = Integer.Parse(e.Node.Name)
+            currentNode = allNodes.Item(key)
+
+            TextBoxCode.Text = currentNode.Code
+            TextBoxName.Text = currentNode.Name
+            TextBoxSeqNo.Text = currentNode.SeqNo
+
+            CheckBoxParent.Enabled = currentNode.AccountCharts.Count = 0 AndAlso currentNode.Sons.Count = 0
+
+            CheckBoxParent.Checked = currentNode.Summary
+            CheckBoxTotal.Checked = currentNode.Total
+
+            CheckBoxTotal.Enabled = currentNode.AccountCharts.Count = 0 AndAlso currentNode.Summary
+
+            CheckBoxDebit.Checked = currentNode.Debit = DareAvere.Dare
+            CheckBoxActive.Checked = currentNode.Active
+
+            If (Not IsNothing(e.Node.Parent)) Then
+                ComboBoxParent.Text = e.Node.Parent.Tag.CodeName
+            Else
+                ComboBoxParent.Text = ""
+            End If
+
+            ComboBoxParent.Enabled = currentNode.NodeType = NodeType.ALTRO
+
+            ButtonNew.Enabled = currentNode.NodeType = NodeType.ALTRO
+            ButtonSave.Enabled = currentNode.NodeType > NodeType.ROOT
+            ButtonDel.Enabled = currentNode.NodeType = NodeType.ALTRO AndAlso currentNode.AccountCharts.Count = 0 AndAlso currentNode.Sons.Count = 0
+
+        Catch ex As Exception
+            Trace.WriteLine(ex.Message)
+        End Try
+    End Sub
+
+    'Private Sub TreeView1_Click(sender As Object, e As EventArgs) Handles TreeView1.Click
+    '    'ClearBackColor()
+    'End Sub
+
+
+    'Private Sub ClearBackColor()
+
+    '    Dim nodes As TreeNodeCollection = TreeView1.Nodes
+    '    For Each n In nodes
+    '        ClearRecursive(n)
+    '    Next
+
+    'End Sub
+
+    '' called by ClearBackColor function
+    'Private Sub ClearRecursive(treeNode As TreeNode)
+    '    For Each tn In treeNode.Nodes
+    '        tn.BackColor = Color.White
+    '        ClearRecursive(tn)
+    '    Next
+    'End Sub
+
+
+    Private Sub CheckBoxParent_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBoxParent.CheckedChanged
+        Dim c As CheckBox = CType(sender, CheckBox)
+        If (Not c.Checked) Then
+            CheckBoxTotal.Checked = False
+        End If
+
+        CheckBoxTotal.Enabled = c.Checked
+
+    End Sub
+
+    Function oneSon(son As AccountCee, level As Integer) As Boolean Implements Traverse.oneSon
+        Return True
+    End Function
+    Function oneParent(parent As AccountCee, level As Integer) As Boolean Implements Traverse.oneParent
+        Return parent.ID <> currentNode.ID
+    End Function
+
+    Private Sub ComboBoxParent_SelectedValueChanged(sender As Object, e As EventArgs) Handles ComboBoxParent.SelectedValueChanged
+        If (IsNothing(currentNode) OrElse currentNode.isLeaf) Then
+            Return
+        End If
+
+        Dim ac = TreeView1.Nodes.Find(ComboBoxParent.SelectedValue.ToString, True)
+        If (ac.Count <> 1) Then
+            MessageBox.Show("Il padre non è presente nell'albero", "Padre non valido")
+            ComboBoxParent.SelectedValue = currentNode.ParentID.ToString
+            Return  'nodo padre non trovato nell'albero
+        End If
+
+        Dim newParent As AccountCee = ac(0).Tag
+
+        If (Not newParent.traverseParents(Me)) Then
+            MessageBox.Show("Il padre non può essere anche un figlio", "Padre non valido")
+            ComboBoxParent.SelectedValue = currentNode.ParentID.ToString
+        End If
+
+    End Sub
+
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
+
+
+        If (Not assigneCurrent()) Then
+            Return
+        End If
+
+        Dim newRec As Boolean = IsNothing(currentNode.ID) OrElse currentNode.ID = 0
+        controller.save(currentNode)
+        If (newRec) Then
+            Dim n As AccountCee = controller.getFromValue(currentNode.Code)
+            If (Not IsNothing(n)) Then
+                allNodes.Add(n.ID, n)
+                AddNode(n)
+                '                TreeView1.SelectedNode.ExpandAll()
+                TreeView1.Refresh()
+            End If
+        End If
+    End Sub
+
+    Private Sub ButtonDel_Click(sender As Object, e As EventArgs) Handles ButtonDel.Click
+
+        controller.remove(currentNode.ID)
+        If (allNodes.ContainsKey(currentNode.ID)) Then
+            allNodes.Remove(currentNode.ID)
+
+            TreeView1.Nodes.RemoveByKey(currentNode.ID)
+            'TreeView1.Refresh()
+            'treeView1.SelectedNode.Remove();
+
+
+        End If
+
+    End Sub
+
+    Private Sub ButtonNew_Click(sender As Object, e As EventArgs) Handles ButtonNew.Click
+
+        currentNode = New AccountCee()
+
+    End Sub
+
+    Private Function validateErrors() As IEnumerable(Of String)
+        Dim errors = New List(Of String)
+
+        If (String.IsNullOrWhiteSpace(TextBoxCode.Text)) Then
+            errors.Add("Il codice (univoco) è necessario")
+        ElseIf (TextBoxCode.Text.Trim().Length = 0 Or TextBoxCode.Text.Trim().Length > 20) Then
+            errors.Add("Il codice deve essere di almeno 1 carattere e massimo 20.")
+        End If
+
+        If (TextBoxName.Text.Trim().Length < 3 Or TextBoxName.Text.Trim().Length > 60) Then
+            errors.Add("Il nome deve essere di almeno 3 caratteri e massimo 60.")
+        End If
+
+        If (IsNothing(ComboBoxParent.SelectedValue)) Then
+            errors.Add("Il padre è necessario")
+        End If
+
+        'If (controller.codeExist(currentNode.ID, TextBoxCode.Text)) Then
+        '    errors.Add("codice già esistente")
+        'End If
+
+        Return errors
+
+    End Function
+
+
+    Private Function assigneCurrent() As Boolean
+
+        Dim errors = validateErrors()
+        If (errors.Count > 0) Then
+            MsgBox(String.Join(Environment.NewLine, errors.ToArray()))
+            Return False
+        End If
+
+        currentNode.Code = TextBoxCode.Text
+        currentNode.Name = TextBoxName.Text
+        currentNode.SeqNo = TextBoxSeqNo.Text
+        currentNode.Summary = CheckBoxParent.Checked
+        currentNode.Total = CheckBoxTotal.Checked
+        currentNode.Debit = IIf(CheckBoxDebit.Checked, DareAvere.Dare, DareAvere.Avere)
+        currentNode.Active = CheckBoxActive.Checked
+
+        Dim newParent As AccountCee = allNodes.Item(ComboBoxParent.SelectedValue)
+
+        If (IsNothing(currentNode.Parent)) Then
+            newParent.Sons.Add(currentNode)
+        ElseIf (currentNode.ParentID <> newParent.ID) Then
+            currentNode.Parent.Sons.Remove(currentNode)
+            newParent.Sons.Add(currentNode)
+        End If
+
+        currentNode.ParentID = newParent.ID
+        currentNode.Parent = newParent
+
+        Return True
+
+    End Function
+
+    Private Sub TextBoxCode_LostFocus(sender As Object, e As EventArgs) Handles TextBoxCode.LostFocus
+        If (currentNode.ID > 0 AndAlso TextBoxCode.Text = currentNode.Code) Then
+            Return
+        End If
+
+        If (controller.codeExist(currentNode.ID, TextBoxCode.Text)) Then
+            MsgBox("codice già esistente")
+            TextBoxCode.Text = currentNode.Code
+        End If
+
+    End Sub
+
+#Region "Search node"
+    Private Sub ButtonSearch_Click(sender As Object, e As EventArgs) Handles ButtonSearch.Click
+
+        Dim searchString As String = Nothing
+
+        Dim sea As SearchParameter = New SearchParameter()
+        Try
+            If (sea.ShowDialog(Me) = DialogResult.OK) Then
+                searchString = sea.TextBoxSearch.Text
+            End If
+        Finally
+            sea.Dispose()
+        End Try
+
+        ClearBackColor()
+        If (Not String.IsNullOrWhiteSpace(searchString)) Then
+            FindByText(searchString)
+        End If
+    End Sub
+
+    Private Sub FindByText(search As String)
+
+        Dim searchRegEx As String = ".*(?i:(" & search & ")).*"     'case insensitive
+        'Dim searchRegEx As String = ".*(?i:(" & search & ")).*"    'case sensitive
+
+        'Dim searchRegEx As String = ".*?i:" & search & ".*"
+        Dim re As Regex = New Regex(searchRegEx)
+
+        Dim nodes As TreeNodeCollection = TreeView1.Nodes
+        For Each n In nodes
+            FindRecursive(n, re)
+        Next
+
+    End Sub
+
+    Private Sub FindRecursive(treeNode As TreeNode, re As Regex)
+        For Each tn In treeNode.Nodes
+            If (re.Match(tn.Text.ToUpper).Success() OrElse re.Match(tn.name.ToUpper).Success()) Then
+                tn.BackColor = Color.Yellow
+            End If
+
+            FindRecursive(tn, re)
+        Next
+
+    End Sub
+
+#End Region
+
+#Region "Remove BackColor"
+
+    '' recursively move through the treeview nodes
+    '' and reset backcolors to white
+    Private Sub ClearBackColor()
+        Dim nodes As TreeNodeCollection = TreeView1.Nodes
+        For Each n In nodes
+            ClearRecursive(n)
+        Next
+
+    End Sub
+
+
+    ''called by ClearBackColor function
+    Private Sub ClearRecursive(treeNode As TreeNode)
+        For Each tn In treeNode.Nodes
+            tn.BackColor = Color.White
+            ClearRecursive(tn)
+        Next
+    End Sub
+
+#End Region
+
+
+
+End Class
